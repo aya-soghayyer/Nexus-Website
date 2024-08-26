42619090fa8b10412e573cd9d3a4e61d0d6ddad8
aya-soghayyer <128791822+aya-soghayyer@users.noreply.github.com>
2024-08-26 18:43:06 +0200

    navbar still needs edit

--- Frontend/nexus-frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/nexus-frontend/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import './Navbar.module.css'
+import "./Navbar.module.css";
+// import faker from "faker";
+import image from "../../images/letter_N_logo.png";
+
 function Navbar() {
+    // let image = "f../../images/letter_N_logo.png";
+
     return (
         <>
             <nav className="navbar">
                 <div className="container-fluid">
-                    <NavLink className="navbar-brand" href="#">Navbar</NavLink>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" to="/">Event</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/About">About us </NavLink>
-                            </li>
-                        </ul>
-                    </div>
+                    <table>
+                        <tr>
+                            <td>
+                                <NavLink className="navbar-brand" to="/">
+                                    <img src={image} alt="Nexus photo" />
+                                </NavLink>
+                            </td>
+                            <td>
+                                <div className="collapse">
+                                    <ul className="navbar-nav">
+                                        <li className="nav-item">
+                                            <NavLink
+                                                className="nav-link active"
+                                                aria-current="page"
+                                                to="/"
+                                            >
+                                                Event
+                                            </NavLink>
+                                        </li>
+                                        <li className="nav-item">
+                                            <NavLink className="nav-link" to="/About">
+                                                About us{" "}
+                                            </NavLink>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </nav>
         </>
